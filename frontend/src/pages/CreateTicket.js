31b31a132644e48f756b333a38e301f7344f5163
Vitor_Fernandes <90069323+vitor502@users.noreply.github.com>
2025-04-30 20:42:04 -0300

    Add frontend/src/pages/CreateTicket.js 1746056523570

---
+++ frontend/src/pages/CreateTicket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateTicket = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Create Ticket Page</h1>
+    </div>
+  );
+};
+
+export default CreateTicket;
